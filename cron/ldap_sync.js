2829ee2e6565f999c999266a3c00e5422f54cb44
lager <machv@cesnet.cz>
2018-02-23 21:05:14 +0100

    implement realm history in database

--- cron/ldap_sync.js
+++ cron/ldap_sync.js
@@ -39,42 +39,35 @@ function search_realms(client, database)
     assert.ifError(err);
 
     res.on('searchEntry', function(entry) {
-      // debug
-      //console.log(entry.object);
-
       var dict = {};
       dict.managers = [];
       dict.realms = [];
 
-      if(typeof(entry.object['cn']) === 'object') {	// multiple realms
-	dict.realms = entry.object['cn']
-      }
+      if(typeof(entry.object['cn']) === 'object')	// multiple realms
+        dict.realms = entry.object['cn']
       else  // one realm only
-	dict.realms.push(entry.object['cn'])
+        dict.realms.push(entry.object['cn'])
 
       dict.dn = entry.object['dn'];
       dict.connection_status = entry.object['eduroamConnectionStatus'];
       dict.type = entry.object['eduroamMemberType'];
- 
+
       if(typeof(entry.object['manager']) === 'object')	// multiple managers
-	dict.managers = entry.object['manager'];
+        dict.managers = entry.object['manager'];
       else  // one manager only
-	dict.managers.push(entry.object['manager']);
+        dict.managers.push(entry.object['manager']);
 
       dict.testing_id = Boolean(entry.object['eduroamTestingId']);
       dict.xml_url = Boolean(entry.object['labeledUri']);
 
-      // TODO
       if(entry.object['oPointer'])
-	dict.org_ptr = entry.object['oPointer'];
+        dict.org_ptr = entry.object['oPointer'];
 
-      // TODO
       if(entry.object['eduroamConnected'])
-	dict.connection_timestamp = ldap2date.parse(entry.object['eduroamConnected']);
+        dict.connection_timestamp = ldap2date.parse(entry.object['eduroamConnected']);
 
-      // TODO
       if(entry.object['eduroamRegistered'])
-	dict.register_timestamp = ldap2date.parse(entry.object['eduroamRegistered']);
+        dict.register_timestamp = ldap2date.parse(entry.object['eduroamRegistered']);
 
       dict.last_change = new Date();
 
@@ -87,15 +80,10 @@ function search_realms(client, database)
 
     res.on('end', function(result) {
       search_radius_servers(client, ret, database, function() {
-	//console.log(ret);
         search_orgs(client, ret, database, function(data) {
-	  // debug
-	  //console.log("======================");
-	  //console.log(data);
-
-	  search_managers(client, data, database);
-	  update_realms(data, database);
-	});
+          search_managers(client, data, database);
+          update_realms(data, database);
+        });
       });
     });
   });
@@ -105,13 +93,64 @@ function search_realms(client, database)
 // --------------------------------------------------------------------------------------
 function update_realms(data, database)
 {
-  for(var i in data) {
-    database.realms.update({ dn : data[i].dn }, data[i], { upsert : true },
-    function(err, result) {
-      if(err)
-        console.error(err);
+  async.each(data, function(item, callback) {
+    database.realms.aggregate([{ $match : { dn: item.dn } },        // match item by dn
+                               { $sort : { last_change : -1 } },    // sort by last_change timestamp
+                               { $limit : 1 }                       // get only the newest record
+    ],
+      function(err, results) {
+        if(results.length > 0) {   // something found
+
+          // compare record for insertion and the database record, if they differ, insert record
+          if(compare_records(results[0], item)) {
+
+            database.realms.update({ dn : item.dn, last_change : item.last_change}, item, { upsert : true },        // nothing matches search criteria, this is actually an insert
+            function(err, result) {
+              if(err)
+                console.error(err);
+              callback();
+            });
+          }
+        }
+        else {                     // nothing found, insert new data
+          database.realms.update({ dn : item.dn, last_change : item.last_change}, item, { upsert : true },        // nothing matches search criteria, this is actually an insert
+          function(err, result) {
+            if(err)
+              console.error(err);
+            callback();
+          });
+        }
     });
+  }, function(err) {
+      if(err)
+    console.log(err);
+  });
+}
+// --------------------------------------------------------------------------------------
+// compare two records
+// --------------------------------------------------------------------------------------
+function compare_records(a, b)
+{
+  // TODO - managers?
+  // TODO - realms?
+  // TODO - dalsi veci?
+  var items = ["connection_status", "connection_timestamp", "radius", "register_timestamp", "testing_id", "type", "xml_url" ]
+
+  for(var i in items) {
+    if(a[items[i]] != undefined && b[items[i]] != undefined) {      //  both are defined
+      if(a[items[i]].toString() != b[items[i]].toString()) {        // convert to string and the compare - date comparsion issues
+        return true;     // records differ
+      }
+   }
+    else {
+      if(a[items[i]] == undefined && b[items[i]] == undefined)      //  both are UNdefined
+        return false;        // records do NOT differ
+
+      return true;     // records differ
+    }
   }
+
+  return false;        // records do NOT differ
 }
 // --------------------------------------------------------------------------------------
 // search ldap for managers
@@ -178,21 +217,21 @@ function search_radius_servers(client, data, database, done)
       assert.ifError(err);
 
       res.on('searchEntry', function(entry) {
-	var dict = {};
-	dict.mon_realms = [];
+        var dict = {};
+        dict.mon_realms = [];
 
-	if(!entry.object['eduroamMonRealm'] && !entry.object['eduroamInfRealm'])
-	  item.radius = false;		// is NOT set
-	else
-	  item.radius = true;		// is set
+        if(!entry.object['eduroamMonRealm'] && !entry.object['eduroamInfRealm'])
+          item.radius = false;		// is NOT set
+        else
+          item.radius = true;		// is set
       });
 
       res.on('error', function(err) {
-	console.error('error: ' + err.message);
+        console.error('error: ' + err.message);
       });
 
       res.on('end', function(result) {
-	callback();
+        callback();
       });
     });
   }, function(err) {
@@ -207,49 +246,33 @@ function search_radius_servers(client, data, database, done)
 function search_orgs(client, data, database, done)
 {
   async.eachOf(data, function(item, index, callback) {
-
-    //console.log(item['org_ptr']);
-    //console.log(item['org_ptr'] != undefined);
-
-    // TODO ?
     if(item['org_ptr'] != undefined) {		// org pointer is defined
-
       var opts = {
-	//filter: "dn=" + item['org_ptr'],	// filter by item name pointer
-	scope: 'base',
-	attributes: [ 'o;lang-cs' ]
+        scope: 'base',
+        attributes: [ 'o;lang-cs' ]
       };
 
       client.search(item['org_ptr'], opts, function(err, res) {		// use pointer as search base
-	assert.ifError(err);
+        assert.ifError(err);
 
-	res.on('searchEntry', function(entry) {
-	  // debug
-	  //console.log("found something");
+        res.on('searchEntry', function(entry) {
+          if(entry.object['o;lang-cs']) {
+            data[index]['org_name'] = entry.object['o;lang-cs'];
+          }
+        });
 
-	  // TODO
-	  if(entry.object['o;lang-cs']) {
-	    data[index]['org_name'] = entry.object['o;lang-cs'];
+        res.on('error', function(err) {
+          console.error('error: ' + err.message);
+        });
 
-	    // debug
-	    //console.log(item['org_ptr']);
-	  }
-	});
-
-	res.on('error', function(err) {
-	  console.error('error: ' + err.message);
-	});
-
-	res.on('end', function(result) {
-	  callback();
-	});
+        res.on('end', function(result) {
+          callback();
+        });
       });
     }
     else		// org pointer NOT defined
       callback();
   }, function(err) {
-      //console.log(data);
-
       if(err)
         console.log(err);
       done(data);		// all items are done
