d611378fbfb4a4b2fdc00bfffba27edf65873717
lager <machv@cesnet.cz>
2018-03-08 11:58:48 +0100

    use exports directory for serving files

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // --------------------------------------------------------------------------------------
 app.use('/', index);
+app.use('/export', express.static('public/export'));
 
 // --------------------------------------------------------------------------------------
 // catch 404 and forward to error handler
