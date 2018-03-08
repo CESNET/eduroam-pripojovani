5491f797ccc677f0bb5866fc5266ce3ba0c6ea13
lager <machv@cesnet.cz>
2018-03-08 11:57:06 +0100

    add export cron task

--- cron.js
+++ cron.js
@@ -2,12 +2,15 @@ module.exports = function(database) {
 // --------------------------------------------------------------------------------------
   const CronJob = require('cron').CronJob;
   const ldap_sync = require('./cron/ldap_sync.js');
+  const exp = require('./cron/export.js');
 // --------------------------------------------------------------------------------------
   // run once a day
 // --------------------------------------------------------------------------------------
-  //ldap_sync.synchronize_data(database);
   new CronJob('0 00 05 * * *', function() {     // run at 05:00:00
-    ldap_sync.synchronize_data(database);
+    ldap_sync.synchronize_data(database,
+      function() {      // export data after sync is done
+        exp.export_data(database);
+    });
   }, null, true, 'Europe/Prague');
 }
 // --------------------------------------------------------------------------------------
---
+++ cron/export.js
@@ -0,0 +1,135 @@
+var fs = require('fs');
+// --------------------------------------------------------------------------------------
+var exp = {}
+// --------------------------------------------------------------------------------------
+// export data
+// --------------------------------------------------------------------------------------
+exp.export_data = function (database) {
+  const filename_base = "/home/etlog/connection/public/export/export";
+
+  export_json(database, filename_base);
+  export_csv(database, filename_base);
+};
+// --------------------------------------------------------------------------------------
+// export data to json
+// --------------------------------------------------------------------------------------
+function export_json(database, filename_base)
+{
+  var data = [];
+  var stream = database.realms.find({ connection_status : "connected" },
+    { _id : 0, org_name : 1, realms : 1, type : 1, connection_timestamp : 1, org_ico : 1}).cursor({ batchSize: 1000 });
+
+  stream.on('error', function (err) {
+    console.log(err);
+  });
+
+  stream.on('data', function(item) {
+    data.push(item.toObject());     // use plain object not mongoose object !!!!!
+  });
+
+  stream.on('end', function(items) {
+    save_json_data(filename_base, "json", convert_timestamps(data));
+  });
+}
+// --------------------------------------------------------------------------------------
+// export data to csv
+// --------------------------------------------------------------------------------------
+function export_csv(database, filename_base)
+{
+  var data = [];
+  var stream = database.realms.find({ connection_status : "connected" },
+    { _id : 0, org_name : 1, realms : 1, type : 1, connection_timestamp : 1, org_ico : 1, org_id : 1}).cursor({ batchSize: 1000 });
+
+  stream.on('error', function (err) {
+    console.log(err);
+  });
+
+  stream.on('data', function(item) {
+    data.push(item.toObject());     // use plain object not mongoose object !!!!!
+  });
+
+  stream.on('end', function(items) {
+    save_csv_data(filename_base, "csv", convert_to_csv(data));
+  });
+}
+// --------------------------------------------------------------------------------------
+// save json data into specified file
+// --------------------------------------------------------------------------------------
+function save_json_data(file, extension, data)
+{
+  fs.writeFile(file + "." + extension, JSON.stringify(data), 'utf8', function(err) {
+    if(err)
+      return console.log(err);
+  });
+}
+// --------------------------------------------------------------------------------------
+// save csv data into specified file
+// --------------------------------------------------------------------------------------
+function save_csv_data(file, extension, data)
+{
+  fs.writeFile(file + "." + extension, data, 'utf8', function(err) {
+    if(err)
+      return console.log(err);
+  });
+}
+// --------------------------------------------------------------------------------------
+// convert timestamps
+// --------------------------------------------------------------------------------------
+function convert_timestamps(data)
+{
+  for(var i in data)
+    if(data[i].connection_timestamp)
+      data[i].connection_timestamp = convert_to_local_date_string(data[i].connection_timestamp);
+
+  return data;
+}
+// --------------------------------------------------------------------------------------
+// convert to csv
+// --------------------------------------------------------------------------------------
+function convert_to_csv(data)
+{
+  var items = [ "org_name", "realms", "type", "connection_timestamp", "org_ico", "org_id" ];
+  var ret = "";
+  ret += "org_name, realms, type, connection_timestamp, org_ico, org_id";
+  ret += "\n";
+
+  data = convert_timestamps(data);
+
+  for(var i in data) {
+    for(var j in items) {
+      if(data[i][items[j]] && typeof(data[i][items[j]]) === 'object' && data[i][items[j]].length > 1) {     // if defined and is array with more than 1 item
+        ret += '"';
+
+        for(var k = 0; k < data[i][items[j]].length; k++) {
+          if(k != data[i][items[j]].length - 1)
+            ret += data[i][items[j]][k] + ", ";
+          else
+            ret += data[i][items[j]][k];
+        }
+
+        ret += '", ';
+      }
+      else if(data[i][items[j]])     // if defined
+        ret += '"' + data[i][items[j]] + '", ';
+
+      else      // not defined
+        ret += ", ";
+    }
+    ret += "\n";
+  }
+
+  ret += "\n";
+  return ret;
+}
+// --------------------------------------------------------------------------------------
+// convert input to local time and return as string
+// --------------------------------------------------------------------------------------
+function convert_to_local_date_string(input)
+{
+  var ret = "";
+  var tmp = input.toISOString();
+  return tmp.replace(/T.*$/, '').split('-')[2] + ". " + tmp.replace(/T.*$/, '').split('-')[1] + ". " +  tmp.replace(/T.*$/, '').split('-')[0];
+}
+// --------------------------------------------------------------------------------------
+module.exports = exp;
+
